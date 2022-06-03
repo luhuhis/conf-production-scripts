8a5b692d1096e48d2b15d3f415a91c3e37c30ed0
Luis Altenkort <altenkort@physik.uni-bielefeld.de>
2022-06-03 15:43:44 +0200

    submit_perlmutter.sh: do both Nts in one file

--- submit_perlmutter.sh
+++ submit_perlmutter.sh
@@ -2,36 +2,42 @@
 
 # File to run RHMC on perlmutter
 
-#Nt=32
-Nt=56
+Nts=(36 56)
+nodes=(3 6)
+Nodes=("1 3 4 1" "1 4 6 1")
+no_updates=(5 3)
+streams=("streams36" "streams56")
+seeds=("seeds36" "seeds56")
 
-#streams=("a" "b" "c" "d")  # Nt32
-streams=("b" "c")  # Nt56
+streams36=("a" "b" "c" "d")
+streams56=("b" "c")
+seeds36=(15619 98789 87918 27180 )
+seeds56=(51070 58544)
 
-#nodes=3  # Nt32
-nodes=4  # Nt56
 
-#Nodes="1 3 4 1"  # Nt32
-Nodes="1 4 4 1"  # Nt56
-
-#seeds=(15619 98789 87918 27180 )  # Nt32
-seeds=(51070 58544)  # Nt56
-
-for idx in "${!streams[@]}" ; do
+for idx in "${!Nts[@]}"; do
+    this_streams="${streams[idx]}"
+    this_seeds="${seeds[idx]}"
+    declare -n this_streams this_seeds  # convert these variables to a reference of the variables their value names
+    for idy in "${!this_streams[@]}" ; do
 ./run_RHMC.sh \
 --module_load cudatoolkit/11.5 craype-accel-nvidia80 PrgEnv-gnu gcc/10.3.0 \
 --output_base_path /pscratch/sd/l/laltenko/conf \
 --executable_dir /global/homes/l/laltenko/code/SIMULATeQCD/build_gnu/applications \
---conftype l96${Nt}f21b8249m002022m01011 \
---stream_id ${streams[idx]} \
---Lattice 96 96 96 ${Nt} \
---Nodes $Nodes \
+--conftype l96${Nts[idx]}f21b8249m002022m01011 \
+--stream_id ${this_streams[idy]} \
+--Lattice 96 96 96 ${Nts[idx]} \
+--Nodes ${Nodes[idx]} \
 --beta 8.249 --mass_s 0.01011 --mass_ud 0.002022 \
 --rat_file /pscratch/sd/l/laltenko/conf/in.rational_n96b8249m002022m01011 \
---seed ${seeds[idx]} \
---jobname RHMC_96${Nt}${streams[idx]} --mail_user laltenkor@bnl.gov \
---time 06:00:00 --nodes $nodes --gpuspernode 4 --array "0-100%1" \
+--seed ${this_seeds[idy]} \
+--jobname RHMC_96${Nts[idx]}${this_streams[idy]} --mail_user laltenkor@bnl.gov \
+--time 06:00:00 --nodes ${nodes[idx]} --gpuspernode 4 --array "0-100%1" \
 --account m3760_g --qos regular --constrain gpu \
---custom_cmds "export MPICH_GPU_SUPPORT_ENABLED=1;"
+--custom_cmds "export MPICH_GPU_SUPPORT_ENABLED=1;" \
+--no_updates ${no_updates[idx]}
+    done
+    # unset the variable references
+    unset -n this_streams
+    unset -n this_seeds
 done
-
