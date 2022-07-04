5cf838e64f60400fd293644cf60cf40f97fe30af
Luis Altenkort <laltenko@qcdi2001.jlab.org>
2022-07-04 09:02:29 -0400

    fix line break bug

--- run_RHMC.sh
+++ run_RHMC.sh
@@ -67,7 +67,7 @@ param_arrays = parser.add_argument_group('Parameters with either a single OR n_s
 
 # GENERAL PARAMETERS
 parser.add_argument('--code', default="SIMULATeQCD", choices=["SIMULATeQCD", "patrick"], help="patrick: change parameter file to use patricks cpu code and do not use GPUs in slurm")
-parser.add_argument('--ConfCheck_path', type=str, help="if provided and conf_nr=auto, then first ConfCheck is used to check whether the last conf is ok.
+parser.add_argument('--ConfCheck_path', type=str, help="if provided and conf_nr=auto, then first ConfCheck is used to check whether the last conf is ok. \
                                               if it is not ok, then it will try the second to last conf.")
 parser.add_argument('--module_load', nargs='*', help="modules will be loaded at the start of the sbatch script. example: --module_load gcc8 cmake3 cuda11")
 parser.add_argument('--output_base_path', required=True, help="folder that will contain the output")
